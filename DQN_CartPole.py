ae1de1ba7d5469403ab62cf5db71daeb0c22e656
Leon Sergio Dela Cruz <leonsergio.delacruz@gmail.com>
2022-05-24 21:19:47 +0800

    Fixed comments and removed unnecessary variables

--- DQN_CartPole.py
+++ DQN_CartPole.py
@@ -1,22 +1,22 @@
-# Second attempt at implementing DQN to the CartPole environment
+'''
+Implementation of a Deep Q Learning Agent for the Cart Pole v0 
+environment of OpenAI.
 
-# Rewriting the code to clean it up
+We use the Double DQN for better stability compared to the 
+normal DQN. The Double DQN using two networks, the main network 
+and the target network. The target network is used as the reference 
+for the training of the main network. This is done for short regular
+intervals (in this case, every 5 episodes) then the target network weights 
+are updated based on the main network weights.
+'''
 
+#import the necessary libraries
 import gym
 import numpy as np
-from matplotlib import pyplot as plt
 import tensorflow as tf
 import random
 
-#create function to play random games. Used to inspect the information
-#acquired from the cartpole environment such as observations,
-#rewards, done, info
-
-epsilon = 1.0
-epsilon_f = 0.1
-epsilon_decay = 0.995
-batch_size = 64
-memory = []
+#define the necessary functions
 def random_games(states,actions,rewards,next_states,dones):
     for episode in range(10): # One game is 1 episode. Play for 10 games
         state = env.reset()
@@ -47,29 +47,12 @@ def create_model(a_space):
                   metrics = tf.keras.metrics.Accuracy())
     return model
 
-
-#create the epsilon-decay greedy policy which prioritizes exploration
-#initially but slowly plrioritizes exploitation as the agent gets better
-
 '''
-def epsilon_greedy_policy(model,state,j,decay): #inputs a counter that will be the decay variable
-    epsilon_i = 1.0
-    epsilon_f = 0.1
-    epsilon = epsilon_f + (epsilon_i - epsilon_f)*(decay**j)
-
-    #generate random number
-    p = np.random.random()
-
-    if p < epsilon: #event with probablity epsilon (initially 100%) (exploration)
-        return np.random.randint(2), epsilon #return random action (0 or 1)
-    else: #exploitation
-        return np.argmax(model.predict(state[np.newaxis,...])), epsilon
+create a function that allows the model to play some games
+and store the <s_o,a_o,r_1,s_1> info from the game
+same structure as random_games function but instead uses the model
+to decide the next action
 '''
-
-# create a function that allows the model to play some games
-# and store the <s_o,a_o,r_1,s_1> info from the game
-# same structure as random_games function but instead uses the model
-# to decide the next action
 
 def practice_game(states,actions,rewards,new_states,dones,model,epsilon,memory):
     while len(states) < batch_size: #play practice games until memory batch is filled
@@ -104,11 +87,11 @@ def practice_game(states,actions,rewards,new_states,dones,model,epsilon,memory):
         epsilon *= epsilon_decay
     return memory_batch, epsilon
 
-
-# define function that recalls portions of the information from the practice
-# games and learns from it. This is where the Bellman Optimality Equation is
-# applied and the targets calculated
-
+'''
+ define function that recalls portions of the information from the practice
+ games and learns from it. This is where the Bellman Optimality Equation is
+ applied and the targets calculated
+'''
 
 def experience_replay(memory_batch,model,train):
     gamma = 0.9 #decay factor gamma
@@ -133,13 +116,14 @@ def experience_replay(memory_batch,model,train):
             targets[i,np.argmax(targets[i])] = reward[i] + gamma*np.max(new_targets[i])
 
     #after acquiring targets, train the model
-    print("state shape for fitting ", state.shape)
-    print("targets shape for fitting ", targets.shape)
-    model.fit(state,targets,epochs = 3)
+    model.fit(state,targets,epochs = 1)
     return model
 
-# play one game that evaluates total score achieved. used to check
-# performance after every training episode
+
+'''
+play 10 games that evaluates the average total score achieved.
+used for checking the performance of the model after every episode
+'''
 
 def evaluation_game(model):
     total_rewards = []
@@ -156,15 +140,27 @@ def evaluation_game(model):
                 break
     return np.array(total_rewards).mean()
 
+#Defining the global variables
+
+'''
+We want the agent to first prioritize exploration then slowly
+prioritize exploitation, thus the initial epsilon, final epsilon values,
+and decay rate are defined below
+'''
+epsilon = 1.0 #initial epsilon value
+epsilon_f = 0.1 #final epsilon value
+epsilon_decay = 0.995 #decay rate of epsilon
 
 
-# Reinforcement Learning Program Proper
+batch_size = 64 #the size of the training data
+memory = [] #for storing the entire experience of the agent
 
 #create the Cart Pole v0 environment
 env = gym.make("CartPole-v0")
 
-a_space = env.action_space.n
+a_space = env.action_space.n #possible actions of the agent based on the environment
 
+#create the main and target networks
 main = create_model(a_space)
 target = create_model(a_space)
 
@@ -174,10 +170,10 @@ print("Before training, score is: ", evaluation_game(main))
 #Training proper
 
 episodes = 1000 #train for 1000 episodes
-decay = 0.995 #decay rate for epsilon 
 
-j = 0 #counter for the epsilon decay
-i = 0 #couhter for target update
+i = 0 #counter for target update
+update_target_interval = 5 #we update the target network based on the main network weights every 5 episodes
+
 for episode in range(episodes):
     #initate variables
     states,actions,rewards,new_states,dones = [],[],[],[],[]
@@ -188,21 +184,27 @@ for episode in range(episodes):
     #learn from the memory and train the model
     main = experience_replay(memory_batch,main,target)
 
-    if i == 5:
+    if i == update_target_interval:
         target.set_weights(main.get_weights())
         i = 0
         
     #evaluate thhe performance of the model by getting the total score possible
 
     total = evaluation_game(main)
-    print("Average Total Score: {score} at Episode {episode}".format(score = total, episode = j+1))
+    print("Average Total Score: {score} at Episode {episode}".format(score = total, episode = episode+1))
+
+    '''
+    We use a standard score of 195 to determine if the agent has
+    "solved" the environment. If the evaluation shows a score >= standard,
+    then we stop the training and save the model.
+    '''
 
     if total >= 195:
         main.save("cartpole_2.h5")
         print("Model achieved goal score of >= 195")
         print("Saving model. Exiting training")
         break
-    j += 1
+
     i += 1
 
 
